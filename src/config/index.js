28bfe5a245fbb3de6f8f1c7d655cd53faa7a549f
kevinarsankamto05 <201110097@student.mercubuana-yogya.ac.id>
2024-07-01 21:31:47 +0700

    update config

--- src/config/index.js
+++ src/config/index.js
@@ -3,17 +3,15 @@ dotEnv.config();
 const ImageKit = require("imagekit");
 
 module.exports = {
-    PORT: process.env.PORT,
-    SECRET_KEY: process.env.SECRET_KEY,
-    FORGOT_PASSWORD: process.env.FORGOT_PASSWORD,
-    USER_EMAIL: process.env.USER_EMAIL,
-    USER_PASSWORD: process.env.USER_PASSWORD,
-    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  PORT: process.env.PORT,
+  SECRET_KEY: process.env.SECRET_KEY,
+  FORGOT_PASSWORD: process.env.FORGOT_PASSWORD,
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
 
-    imageKit: new ImageKit({
-        publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
-        privateKey: process.env.IMAGEKIT_SECRET_KEY,
-        urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
-    }),
+  imageKit: new ImageKit({
+    publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+    privateKey: process.env.IMAGEKIT_SECRET_KEY,
+    urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
+  }),
 };
