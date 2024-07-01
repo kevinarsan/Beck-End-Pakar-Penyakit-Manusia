3a32383f9a0cb25e6f1d5857cd789af29351bffb
kevinarsankamto05 <201110097@student.mercubuana-yogya.ac.id>
2024-07-01 22:12:10 +0700

    update router

--- src/index.js
+++ src/index.js
@@ -7,7 +7,14 @@ const express = require("express"),
 const cors = require("cors");
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
